71507ea504e1781b35e1e96f2afee5a98040ee8b
Anum Mushtaq <anum.mushtaq.am96@gmail.com>
2021-03-27 14:59:56 +0100

    Updated

--- src/analysis/task_ols.py
+++ src/analysis/task_ols.py
@@ -8,19 +8,16 @@ from task_data_management import mean_series
 from task_data_management import average_month
 
 
-
-def plotmodel(regression_output,title):
-    filepath= "bld/analysis/"
+def plotmodel(regression_output, title):
+    filepath = "bld/analysis/"
     plt.rc("figure", figsize=(12, 7))
     plt.text(
         0.01, 0.05, regression_output, {"fontsize": 10}, fontproperties="monospace"
     )
     plt.axis("off")
     plt.tight_layout()
-    plt.savefig(filepath+title+'.png')
+    plt.savefig(filepath+title +".png")
     plt.close()
-
-    
 
 
 # OLS wave 1
@@ -45,7 +42,6 @@ print(results.summary())
 plotmodel(str(results.summary()),"model_1")
 
 
-
 # OLS wave 2
 y = incidence_means.iloc[2:3, 1:2].values
 x = [[1], [mean_series.values[11]], [mean_series.values[12]]]
@@ -53,7 +49,6 @@ x = np.array(x).T
 results_2 = sm.OLS(endog=y, exog=x).fit()
 print(results_2.summary())
 plotmodel(str(results_2.summary()),"model_2")
-
 
 
 # OLS wave 3
@@ -74,8 +69,6 @@ print(results_3.summary())
 plotmodel(str(results_3.summary()),"model_3")
 
 
-
-
 # OLS wave 4
 y = incidence_means.iloc[4:5, 1:2].values
 x = [
@@ -91,7 +84,6 @@ print(results_4.summary())
 plotmodel(str(results_4.summary()),"model_4")
 
 
-
 # OLS wave 5
 y = incidence_means.iloc[7:8, 1:2].values
 x = [
@@ -104,7 +96,6 @@ x = np.array(x).T
 results_5 = sm.OLS(endog=y, exog=x).fit()
 print(results_5.summary())
 plotmodel(str(results_5.summary()),"model_5")
-
 
 
 # OLS with log base 10
@@ -130,7 +121,6 @@ print(results_6.summary())
 plotmodel(str(results_6.summary()),"model_6")
 
 
-
 # OLS wave 2
 y = incidence_means.iloc[2:3, 6].values
 x = [[1], [mean_series.values[11]], [mean_series.values[12]]]
@@ -138,7 +128,6 @@ x = np.array(x).T
 results_7 = sm.OLS(endog=y, exog=x).fit()
 print(results_7.summary())
 plotmodel(str(results_7.summary()),"model_7")
-
 
 
 # OLS wave 3 with log base 10
@@ -157,7 +146,6 @@ x = np.array(x).T
 results_8 = sm.OLS(endog=y, exog=x).fit()
 print(results_8.summary())
 plotmodel(str(results_8.summary()),"model_8")
-
 
 
 # OLS wave 4 with log base 10
@@ -189,7 +177,6 @@ print(results_10.summary())
 plotmodel(str(results_10.summary()),"model_10")
 
 
-
 # OLS average across all waves without log
 y = [
     incidence_means.iloc[1:2, 1:2].values,
@@ -210,7 +197,6 @@ x = np.array(x)
 results_11 = sm.OLS(endog=y, exog=x).fit()
 print(results_11.summary())
 plotmodel(str(results_11.summary()),"model_11")
-
 
 
 # OLS Average across all the five waves with log base 10
@@ -234,10 +220,3 @@ x = np.array(x)
 results_12 = sm.OLS(endog=y, exog=x).fit()
 print(results_12.summary())
 plotmodel(str(results_12.summary()),"model_12")
-
-
-
-
-
-
-
